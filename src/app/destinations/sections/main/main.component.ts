bbb288c3104537a3f62b7ad0884bfc9b921de8cf
José Gilabert <56225245+JGP84@users.noreply.github.com>
2024-02-11 22:08:54 +0100

    create a layout & restyling search section

--- src/app/destinations/sections/main/main.component.ts
+++ src/app/destinations/sections/main/main.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'destinations-main',
   templateUrl: 'main.component.html',
+  styleUrls: ['main.component.css']
 })
 export class MainComponent implements OnInit {
   constructor() {}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,11 +4,23 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+
 @NgModule({
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatDividerModule,
+    MatListModule
   ],
 })
 export class MaterialModule {}
