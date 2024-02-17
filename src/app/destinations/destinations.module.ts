9484c10229cb90b37303eafee136c0b5660cd618
José Gilabert <56225245+JGP84@users.noreply.github.com>
2024-02-17 22:18:52 +0100

    delete sidebar section

--- src/app/destinations/destinations.module.ts
+++ src/app/destinations/destinations.module.ts
@@ -4,27 +4,34 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './sections/navbar/navbar.component';
 import { SearchComponent } from './sections/search/search.component';
 import { MainComponent } from './sections/main/main.component';
-import { SidebarComponent } from './sections/sidebar/sidebar.component';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
 import { RouterModule } from '@angular/router';
 import { TruncatePipe } from './pipes/truncate.pipe';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-
-
 @NgModule({
-  imports: [CommonModule, MaterialModule, RouterModule, BrowserAnimationsModule],
-  exports: [MaterialModule, NavbarComponent, SearchComponent, MainComponent, SidebarComponent, CardComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [
+    MaterialModule,
+    NavbarComponent,
+    SearchComponent,
+    MainComponent,
+    CardComponent,
+  ],
 
   declarations: [
     NavbarComponent,
     SearchComponent,
     MainComponent,
-    SidebarComponent,
+
     CardComponent,
-    TruncatePipe
+    TruncatePipe,
   ],
 
   providers: [],
