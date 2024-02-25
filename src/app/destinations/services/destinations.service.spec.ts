00d9cb6de69a06fb84894c826b3d3de126dff7a8
José Gilabert <56225245+JGP84@users.noreply.github.com>
2024-02-25 22:24:21 +0100

    refactor tests in destination service

--- src/app/destinations/services/destinations.service.spec.ts
+++ src/app/destinations/services/destinations.service.spec.ts
@@ -8,6 +8,11 @@ import { Destin } from '../interfaces/destin.interface';
 import { share, catchError } from 'rxjs/operators';
 import { lastValueFrom, of } from 'rxjs';
 
+import data from '../../../assets/mock/db.json';
+const mockDestinations: Destin[] = data.destinations;
+
+const MOCK_URL = './assets/mock/db.json';
+
 describe('DestinationService', () => {
   let service: DestinationService;
   let httpMock: HttpTestingController;
@@ -26,166 +31,130 @@ describe('DestinationService', () => {
     httpMock.verify();
   });
 
-  it('should be created', async () => {
-    expect(service).toBeTruthy();
-    const req = httpMock.expectOne('/assets/mock/db.json');
-    expect(req.request.method).toBe('GET');
-    await req.flush({ destinations: [] });
-  });
+  describe('fetchDestinations', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
 
-  const mockDestinations: Destin[] = [
-    {
-      name: 'France',
-      capital: 'Paris',
-      country_code: 'FR',
-      id: 'des_FR',
-      description:
-        "France, located in Western Europe, is known for its art, fashion, culture, and cuisine. It's home to many world-renowned landmarks such as the Eiffel Tower, Louvre Museum, and Palace of Versailles.",
-      src_img: 'assets/img/destinations/des_FR.jpg',
-    },
-    {
-      name: 'Spain',
-      capital: 'Madrid',
-      country_code: 'ES',
-      id: 'des_ES',
-      description:
-        "Spain, a country on Europe's Iberian Peninsula, is famous for its vibrant cities, beautiful beaches, and rich history. It's known for its diverse culture, Flamenco music and dance, bullfights and festivals.",
-      src_img: 'assets/img/destinations/des_ES.jpg',
-    },
-    {
-      name: 'Italy',
-      capital: 'Rome',
-      country_code: 'IT',
-      id: 'des_IT',
-      description:
-        'Italy, a European country with a long Mediterranean coastline, has left a powerful mark on Western culture and cuisine. Its capital, Rome, is home to the Vatican as well as landmark art and ancient ruins.',
-      src_img: 'assets/img/destinations/des_IT.jpg',
-    },
-    {
-      name: 'Germany',
-      capital: 'Berlin',
-      country_code: 'DE',
-      id: 'des_DE',
-      description:
-        'Germany, located in Central Europe, is known for its long and rich history, culture, and science. It is home to many world-renowned landmarks such as the Berlin Wall, Brandenburg Gate, and Neuschwanstein Castle.',
-      src_img: 'assets/img/destinations/des_DE.jpg',
-    },
-    {
-      name: 'United Kingdom',
-      capital: 'London',
-      country_code: 'GB',
-      id: 'des_GB',
-      description:
-        'The United Kingdom, located off the northwestern coast of mainland Europe, is known for its history, culture, and global influence. It is home to many world-renowned landmarks such as the Big Ben, Tower of London, and Stonehenge.',
-      src_img: 'assets/img/destinations/des_GB.jpg',
-    },
-  ];
-
-  it('should fetch destinations', async () => {
-    spyOn(service, 'fetchDestinations').and.returnValue(of(mockDestinations));
-
-    service.fetchDestinations().subscribe((destinations) => {
-      expect(destinations.length).toBe(5);
-      expect(destinations).toEqual(mockDestinations);
+      const req = httpMock.expectOne(MOCK_URL);
+      expect(req.request.method).toBe('GET');
+      req.flush({ destinations: [] });
     });
 
-    httpMock.expectOne('/assets/mock/db.json').flush(mockDestinations);
+    it('should fetch destinations', () => {
+      service.fetchDestinations().subscribe();
 
-    expect(service.fetchDestinations).toHaveBeenCalledTimes(1);
-  });
+      const requests = httpMock.match(MOCK_URL);
+      expect(requests.length).toBe(2);
 
-  it('should handle fetch destinations error', async () => {
-    const requestObservable = service.fetchDestinations().pipe(
-      share(),
-      catchError((error) => {
-        expect(error).toBeTruthy();
-        return of([]);
-      })
-    );
-
-    requestObservable.subscribe((destinations) => {
-      expect(destinations.length).toBe(0);
+      requests.forEach((req) => {
+        expect(req.request.method).toBe('GET');
+        req.flush({ destinations: mockDestinations });
+      });
     });
 
-    setTimeout(() => {
-      const requests = httpMock.match('/assets/mock/db.json');
-      if (requests.length > 0) {
-        requests.forEach((req) => {
-          req.flush('Simulated network error', {
-            status: 500,
-            statusText: 'Internal Server Error',
+    it('should handle fetch destinations error', async () => {
+      const requestObservable = service.fetchDestinations().pipe(
+        share(),
+        catchError((error) => {
+          expect(error).toBeTruthy();
+          return of([]);
+        })
+      );
+
+      requestObservable.subscribe((destinations) => {
+        expect(destinations.length).toBe(0);
+      });
+
+      setTimeout(() => {
+        const requests = httpMock.match(MOCK_URL);
+        if (requests.length > 0) {
+          requests.forEach((req) => {
+            req.flush('Simulated network error', {
+              status: 500,
+              statusText: 'Internal Server Error',
+            });
           });
-        });
-      }
-    }, 0);
+        }
+      }, 0);
 
-    await lastValueFrom(requestObservable);
-  });
-
-  it('should delete a destination', async () => {
-    const destinationToDelete: Destin = mockDestinations[0];
-    await service.deleteDestination(destinationToDelete.id);
-
-    const req = httpMock.expectOne('/assets/mock/db.json');
-    req.flush([mockDestinations[1]]);
-
-    service.getDestinations().subscribe((destinations) => {
-      expect(destinations).not.toContain(destinationToDelete);
+      await lastValueFrom(requestObservable);
     });
   });
 
-  it('should update a destination', async () => {
-    const updatedDestination: Destin = {
-      ...mockDestinations[0],
-      name: 'Updated Destination',
-    };
-    await service.updateDestination(updatedDestination);
+  describe('deleteDestination', () => {
+    it('should delete a destination', async () => {
+      const destinationToDelete: Destin = mockDestinations[0];
+      await service.deleteDestination(destinationToDelete.id);
 
-    const index = mockDestinations.findIndex(
-      (d) => d.id === updatedDestination.id
-    );
-    mockDestinations[index] = updatedDestination;
+      const req = httpMock.expectOne(MOCK_URL);
+      req.flush([mockDestinations[1]]);
 
-    const req = httpMock.expectOne('/assets/mock/db.json');
-    req.flush({ destinations: mockDestinations });
+      service.getDestinations().subscribe((destinations) => {
+        expect(destinations).not.toContain(destinationToDelete);
+      });
+    });
+  });
 
-    service.getDestinations().subscribe((destinations) => {
-      const destination = destinations.find(
+  describe('updateDestination', () => {
+    it('should update a destination', async () => {
+      const updatedDestination: Destin = {
+        ...mockDestinations[0],
+        name: 'Updated Destination',
+      };
+      await service.updateDestination(updatedDestination);
+
+      const index = mockDestinations.findIndex(
         (d) => d.id === updatedDestination.id
       );
-      expect(destination).toEqual(updatedDestination);
+      mockDestinations[index] = updatedDestination;
+
+      const req = httpMock.expectOne(MOCK_URL);
+      req.flush({ destinations: mockDestinations });
+
+      service.getDestinations().subscribe((destinations) => {
+        const destination = destinations.find(
+          (d) => d.id === updatedDestination.id
+        );
+        expect(destination).toEqual(updatedDestination);
+      });
     });
   });
 
-  it('should get a destination by id', () => {
-    const destinationToGet: Destin = mockDestinations[0];
-    const req = httpMock.expectOne('/assets/mock/db.json');
-    req.flush({ destinations: mockDestinations });
+  describe('getDestinationById', () => {
+    it('should get a destination by id', () => {
+      const destinationToGet: Destin = mockDestinations[0];
+      const req = httpMock.expectOne(MOCK_URL);
+      req.flush({ destinations: mockDestinations });
 
-    const destination = service.getDestinationById(destinationToGet.id);
-    expect(destination).toEqual(destinationToGet);
+      const destination = service.getDestinationById(destinationToGet.id);
+      expect(destination).toEqual(destinationToGet);
+    });
   });
 
-  it('should add a destination', async () => {
-    const newDestination: Destin = {
-      name: 'New Destination',
-      capital: 'New Capital',
-      country_code: 'ND',
-      id: 'des_ND',
-      description: 'New Destination Description',
-      src_img: 'assets/img/destinations/des_ND.jpg',
-    };
+  describe('addDestination', () => {
+    it('should add a destination', async () => {
+      const newDestination: Destin = {
+        name: 'New Destination',
+        capital: 'New Capital',
+        country_code: 'ND',
+        id: 'des_ND',
+        description: 'New Destination Description',
+        src_img: 'assets/img/destinations/des_ND.jpg',
+      };
 
-    await service.addDestination(newDestination);
+      await service.addDestination(newDestination);
 
-    mockDestinations.push(newDestination);
+      mockDestinations.push(newDestination);
 
-    const req = httpMock.expectOne('/assets/mock/db.json');
-    req.flush({ destinations: mockDestinations });
+      const req = httpMock.expectOne(MOCK_URL);
+      req.flush({ destinations: mockDestinations });
 
-    service.getDestinations().subscribe((destinations) => {
-      const destination = destinations.find((d) => d.id === newDestination.id);
-      expect(destination).toEqual(newDestination);
+      service.getDestinations().subscribe((destinations) => {
+        const destination = destinations.find(
+          (d) => d.id === newDestination.id
+        );
+        expect(destination).toEqual(newDestination);
+      });
     });
   });
 });
